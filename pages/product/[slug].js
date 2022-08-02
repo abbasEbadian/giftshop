2115f79d779c4e3952c3c83011cd2424bd661175
abbasebadian <abbas@vm0089421926.bitcommand.com>
2022-08-02 10:14:48 +0000

    blogs per page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -18,68 +18,66 @@ import Link from "next/link";
 import Button from "@mui/material/Button";
 import * as e from "../../redux/endpoints"
 
-function Product({data}) {
+function Product({ data }) {
   const router = useRouter();
   const pid = router.query.slug;
   const [product, setProduct] = React.useState(undefined);
   const [count, setCount] = React.useState(1);
-  const [loading, setLoading] = React.useState(true)   
-  const [loading1, setLoading1] = React.useState(false)   
-  const [similar, setSimilar] = React.useState(true)   
-  // React.useEffect(() => {
-  //   const p = cards.filter((i) => i.id === +pid);
-  //   if (p) setProduct(p[0]);
-  // }, [cards, pid]);
+  const [loading, setLoading] = React.useState(true)
+  const [loading1, setLoading1] = React.useState(false)
+  const [similar, setSimilar] = React.useState(true)
+  const config = useSelector(s=>s.main.configs)
 
 
-  React.useEffect(()=>{
-    if(!pid) return
+
+  React.useEffect(() => {
+    if (!pid) return
     const pids = pid.split('-')[0]
-    axios.get(GET_CARD + pids).then(res=>{
+    axios.get(GET_CARD + pids).then(res => {
 
-      const {data}  = res
+      const { data } = res
       setProduct(data.card)
       setSimilar(data.similar)
-    }).catch(err=>{
+    }).catch(err => {
       console.log(err)
     })
-    .finally(f=>{
-      setTimeout(()=>{setLoading(false)}, 1000)
-    })
+      .finally(f => {
+        setTimeout(() => { setLoading(false) }, 1000)
+      })
 
   }, [pid])
   const dispatch = useDispatch()
-  const _addToCart = ()=>{
+  const _addToCart = () => {
     setLoading1(true)
 
     axios.post(ADD_TO_CART, {
       template_id: product.id,
       count: count
-    }).then(res=>{
-      const {data}= res 
-      if(data.error === 0)
+    }).then(res => {
+      const { data } = res
+      if (data.error === 0)
         dispatch(get_cart())
-      else if(data.error === 1 && data.message.indexOf('وارد') > -1){
+      else if (data.error === 1 && data.message.indexOf('وارد') > -1) {
         dispatch(update_login_modal(true))
       }
-      toast(data.message, {type: data.type})
+      toast(data.message, { type: data.type })
     })
-    .catch(e=>{
-      console.log(e)
-    })
-    .finally(e=>{
-      setLoading1(false)
-    })
+      .catch(e => {
+        console.log(e)
+      })
+      .finally(e => {
+        setLoading1(false)
+      })
   }
   const [open, setOpen] = React.useState(false)
   return (
     <div className="container single-product ">
-        <Head>
-          <title>{data.meta_title??(product && product.full_name)}</title>
-          <meta name="description" content={data.meta_description??(product && product.full_name)}/>
-          <meta name="keywords" content={data.meta_keywords??(product && product.full_name)}/>
-          {data.meta_canonical? <link rel="canonical" href={data.meta_canonical} /> :null}
-        </Head>
+      <Head>
+        <title>{data.meta_title ?? (product && product.full_name)}</title>
+        <meta name="description" content={data.meta_description ?? (product && product.full_name)} />
+        <meta name="keywords" content={data.meta_keywords ?? (product && product.full_name)} />
+        {data.meta_canonical ? <link rel="canonical" href={data.meta_canonical} /> : null}
+      </Head>
 
       <h2 className="text-center my-4">
         گیفت کارت {" "}
@@ -87,13 +85,13 @@ function Product({data}) {
           <span className="text-secondary">{product?.brand_id?.persian_name || ""}</span>
         </a></Link>
       </h2>
-        {product&& product.brand_id&&product.brand_id.url?
-          <a className="mx-md-5 py-4" href={product.brand_id.url} target="_blank">
-           <Button variant="contained" color="info"> لینک آموزش استفاده</Button>
-          </a>
-         :null}
-        <div className="row  mt-2 product-list-gift mx-md-5">
-          
+      {product && product.brand_id && product.brand_id.url ?
+        <a className="mx-md-5 py-4" href={product.brand_id.url} target="_blank">
+          <Button variant="contained" color="info"> لینک آموزش استفاده</Button>
+        </a>
+        : null}
+      <div className="row  mt-2 product-list-gift mx-md-5">
+
         {product ? <>
           <div className="col-12 col-lg-4  pt-5 pt-md-0">
             <Card data={product} favoriteAndRate />
@@ -105,8 +103,12 @@ function Product({data}) {
                 <span className="text-primary  fs-5 d-block">قیمت</span>
                 <br />
                 <span className="pt-3 mt-3">
+                {(product?.no_sell || product?.ask_me)? "-":
+                  <>
                   {product.price}
                   {" تومان"}
+                  </>}
+                  
                 </span>
               </div>
               <div className="col-6 col-md-3 mb-2">
@@ -123,38 +125,40 @@ function Product({data}) {
                 <span className="text-primary  fs-5 d-block">امتیاز مشتریان</span>
                 <br />
                 <span className="pt-3 mt-3">
-                  {product.rate} {"("+product.reviews_count+")"}
+                  {product.rate} {"(" + product.reviews_count + ")"}
                 </span>
               </div>
             </div>
-            
+
             <div className="add-to-card-container product d-flex justify-content-between align-items-center   me-auto ms-0">
-              <div dir="ltr" className="counter">
-                <span onClick={(e) => setCount((c) => (c += 1))}>+</span>
-                <span className="border-bottom mx-2 ">{count}</span>
-                <span onClick={(e) => setCount((c) => Math.max(1, c - 1))}>
-                  -
-                </span>
-              </div>
-              <span className="border rounded p-2">
-                {product.price} {" تومان "}{" "}
-              </span>
-              <LoaderButton text="افزودن به سبد خرید" className=" px-3" loading={ loading1} onClick={_addToCart}/>
+              {product?.no_sell ? <Button color='error' variant="outlined" className="exclude" fullWidth>ناموجود</Button> :
+                product?.ask_me ? <Link href={config?.contactus?.whatsapp_link || "#"}><a className="text-primary w-100"><Button color='info' variant="outlined" className="exclude" fullWidth>استعلام موجودی</Button></a></Link> :
+                  <>
+                    <div dir="ltr" className="counter">
+                      <span onClick={(e) => setCount((c) => (c += 1))}>+</span>
+                      <span className="border-bottom mx-2 ">{count}</span>
+                      <span onClick={(e) => setCount((c) => Math.max(1, c - 1))}>-</span>
+                    </div>
+                    <span className="border rounded p-2">
+                      {product.price} {" تومان "}{" "}
+                    </span>
+                    <LoaderButton text="افزودن به سبد خرید" className=" px-3" loading={loading1} onClick={_addToCart} />
+                  </>}
             </div>
           </div>
-          </>
-        
-       : loading?<section className="w-75 py-5 my-5 d-flex justify-content-center mx-auto"><Bars/></section>:
-        <section className="w-75 alert alert-info my-5 mx-auto">محصول یافت نشد</section>
-      }</div>
-      <div className="desc product-list-gift mx-md-5 my-4 p-3 blog-desc overflow-hidden position-relative pb-5" style={{height: (open? "unset": (product?.description.length > 1200? "300px" : "max-content"))}}>
-         <p className="mt-3  text-truncate h-100">توضیحات:  <span dangerouslySetInnerHTML={{
-              __html: product?.description
+        </>
+
+          : loading ? <section className="w-75 py-5 my-5 d-flex justify-content-center mx-auto"><Bars /></section> :
+            <section className="w-75 alert alert-info my-5 mx-auto">محصول یافت نشد</section>
+        }</div>
+      <div className="desc product-list-gift mx-md-5 my-4 p-3 blog-desc overflow-hidden position-relative pb-5" style={{ height: (open ? "unset" : (product?.description.length > 1200 ? "300px" : "max-content")) }}>
+        <p className="mt-3  text-truncate h-100">توضیحات:  <span dangerouslySetInnerHTML={{
+          __html: product?.description
         }}></span></p>
-        {product?.description.length > 1200 &&<Button sx={{width: '100%', height: '38px', backgroundColor: '#efe', color: "#4c4c4c"}} onClick={e=>{setOpen(!open)}} className="position-absolute bottom-0 start-0 end-0">
-          {open? 
-          "مشاهده کمتر "
-          : "مشاهده بیشتر"}
+        {product?.description.length > 1200 && <Button sx={{ width: '100%', height: '38px', backgroundColor: '#efe', color: "#4c4c4c" }} onClick={e => { setOpen(!open) }} className="position-absolute bottom-0 start-0 end-0">
+          {open ?
+            "مشاهده کمتر "
+            : "مشاهده بیشتر"}
         </Button>}
       </div>
 
@@ -174,22 +178,22 @@ function Product({data}) {
       <div className="card my-4 p-4">
         <div className="col-12 col-lg-8 co-xl-6 mx-auto">
           <SendFeedback product={product} />
-          <Reviews reviews={product&&product.review_set||[]} />
+          <Reviews reviews={product && product.review_set || []} />
         </div>
       </div>
-     
+
     </div>
   );
 }
-export async function getServerSideProps({query}) {
-  try{
+export async function getServerSideProps({ query }) {
+  try {
     const pid = query.slug.split("-")[0]
     const res = await fetch(e.GET_PRODUCT_TITLE(pid))
     const data = await res.json()
 
     return { props: { data } }
-  }catch(e){
-    return { props: { data:{} } }
+  } catch (e) {
+    return { props: { data: {} } }
   }
 }
 export default Product;
