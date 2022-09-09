e8b597e52901227f7b13b5862af86387eeeabc13
abbasEbadian <abbasebadiann@gmail.com>
2022-09-09 23:31:26 +0430

    pagination slug qery fix

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -8,9 +8,10 @@ import Link from 'next/link';
 export default function PaginationControlled({  page, size=5, countInEachPage = 20, source_url, extra_query={} }) {
   let active = +page;
   let items = [];
+  const {slug, ...query} = extra_query
   for (let number = 1; number <= Math.ceil(size / countInEachPage); number++) {
     items.push(
-          <Link href={{pathname: source_url, query: { ...extra_query, page: number}}} passHref>
+          <Link href={{pathname: source_url, query: {...query, page: number}}} passHref>
       <Pagination.Item key={number} active={number === active} >
           {number}
           {/* <Link ></Link> */}
